0c3e5902a2dfa440d1dad74680175213aa9724b2
“prongbang” <prongbang@gmail.com>
2022-09-29 01:27:18 +0700

    Optimize import

--- app/src/main/java/com/prongbang/androidsecurebiometric/SecureBiometricManagerModule.kt
+++ app/src/main/java/com/prongbang/androidsecurebiometric/SecureBiometricManagerModule.kt
@@ -2,8 +2,6 @@ package com.prongbang.androidsecurebiometric
 
 import com.prongbang.securebiometric.BiometricPromptInfoBuilder
 import com.prongbang.securebiometric.BiometricPromptInfoBuilderImpl
-import com.prongbang.securebiometric.SecureBiometricManager
-import com.prongbang.securebiometric.SecureBiometricPromptManager
 import com.prongbang.securebiometric.cipher.BiometricCryptography
 import com.prongbang.securebiometric.cipher.BiometricKeyStoreCipher
 import com.prongbang.securebiometric.cipher.Cryptography
@@ -14,12 +12,10 @@ import com.prongbang.securebiometric.key.BiometricCryptographyKey
 import com.prongbang.securebiometric.key.CryptographyKey
 import com.prongbang.securebiometric.keypair.BiometricKeyStoreManager
 import com.prongbang.securebiometric.keypair.KeyStoreManager
-import com.prongbang.securebiometric.token.BiometricToken
 import com.prongbang.securebiometric.utility.AndroidBase64Utility
 import com.prongbang.securebiometric.utility.Base64Utility
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
 
